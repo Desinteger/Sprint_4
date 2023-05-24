2fabfe45614359527154bc0f7bc105fa9c60e1a8
Nika Dezhneva <dezhneva201@gmail.com>
2023-05-24 15:22:03 +0300

    fifth commit

--- src/test/java/ru/yandex/praktikum/test/DropDownTest.java
+++ src/test/java/ru/yandex/praktikum/test/DropDownTest.java
@@ -14,8 +14,18 @@ import static ru.yandex.praktikum.config.AppConfig.APP_URL;
 public class DropDownTest {
     private WebDriver webdriver;
 
+    private final static String expectedText1 = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
+    private final static String expectedText2 = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
+    private final static String expectedText3 = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
+    private final static String expectedText4 = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
+    private final static String expectedText5 = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
+    private final static String expectedText6 = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
+    private final static String expectedText7 = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
+    private final static String expectedText8 = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
+
+
     @Before
-    public void setUp () {
+    public void setUp() {
         WebDriverManager.firefoxdriver().clearDriverCache().setup();
         webdriver = new FirefoxDriver();
         webdriver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
@@ -24,33 +34,60 @@ public class DropDownTest {
         objMainPage.scrollToDropdown();
         objMainPage.closePopupButton();
     }
+
     @Test
-    public void isExpectedTextDisplayedInDropdownAfterClickInFirefox() {
+    public void isExpectedTextDisplayedInDropdownAfterClickInFirefox1 () {
         MainPage objMainPage = new MainPage(webdriver);
-
         objMainPage.clickQuestion(0);
-        assertEquals(objMainPage.getAnswerText(0), "Сутки — 400 рублей. Оплата курьеру — наличными или картой.");
+        assertEquals(objMainPage.getAnswerText(0), expectedText1);
+    }
 
+    @Test
+    public void isExpectedTextDisplayedInDropdownAfterClickInFirefox2 () {
+        MainPage objMainPage = new MainPage(webdriver);
         objMainPage.clickQuestion(1);
-        assertEquals(objMainPage.getAnswerText(1), "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.");
+        assertEquals(objMainPage.getAnswerText(1), expectedText2);
+}
 
+    @Test
+    public void isExpectedTextDisplayedInDropdownAfterClickInFirefox3() {
+        MainPage objMainPage = new MainPage(webdriver);
         objMainPage.clickQuestion(2);
-        assertEquals(objMainPage.getAnswerText(2), "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.");
+        assertEquals(objMainPage.getAnswerText(2), expectedText3);
+    }
 
+    @Test
+    public void isExpectedTextDisplayedInDropdownAfterClickInFirefox4() {
+        MainPage objMainPage = new MainPage(webdriver);
         objMainPage.clickQuestion(3);
-        assertEquals(objMainPage.getAnswerText(3), "Только начиная с завтрашнего дня. Но скоро станем расторопнее.");
+        assertEquals(objMainPage.getAnswerText(3), expectedText4);
+    }
 
+    @Test
+    public void isExpectedTextDisplayedInDropdownAfterClickInFirefox5() {
+        MainPage objMainPage = new MainPage(webdriver);
         objMainPage.clickQuestion(4);
-        assertEquals(objMainPage.getAnswerText(4), "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.");
-
+        assertEquals(objMainPage.getAnswerText(4), expectedText5);
+    }
+    @Test
+    public void isExpectedTextDisplayedInDropdownAfterClickInFirefox6() {
+        MainPage objMainPage = new MainPage(webdriver);
         objMainPage.clickQuestion(5);
-        assertEquals(objMainPage.getAnswerText(5), "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.");
+        assertEquals(objMainPage.getAnswerText(5), expectedText6);
+    }
 
+    @Test
+    public void isExpectedTextDisplayedInDropdownAfterClickInFirefox7() {
+        MainPage objMainPage = new MainPage(webdriver);
         objMainPage.clickQuestion(6);
-        assertEquals(objMainPage.getAnswerText(6), "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.");
+        assertEquals(objMainPage.getAnswerText(6), expectedText7);
+}
 
+    @Test
+    public void isExpectedTextDisplayedInDropdownAfterClickInFirefox8() {
+        MainPage objMainPage = new MainPage(webdriver);
         objMainPage.clickQuestion(7);
-        assertEquals(objMainPage.getAnswerText(7), "Да, обязательно. Всем самокатов! И Москве, и Московской области.");
+        assertEquals(objMainPage.getAnswerText(7), expectedText8);
     }
    @After
    public void teardown() { webdriver.quit(); }
